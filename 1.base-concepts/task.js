b1f4eab9f25d09e3119a0ff6688a4adef5bfa987
Elena <elena_golubkova1982@mail.ru>
2026-04-16 17:50:45 +0400

    Перенес файлы в папку 1.base-concepts, поправил пути

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,19 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+  let d = b ** 2 - 4 * a * c;
+  let roots = [];
+  if (d < 0) {
+    return roots;
+  } 
+  else if (d === 0) {
+    let root = -b / (2 * a);
+    roots.push(root);
+  } 
+  else {
+    let root1 = (-b + Math.sqrt(d)) / (2 * a);
+    let root2 = (-b - Math.sqrt(d)) / (2 * a);
+    roots.push(root1, root2);
+  }
+  return roots;
 }
